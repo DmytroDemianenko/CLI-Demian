de962ec1b4f06f6dce374cb94c7164181c41b9d6
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2022-04-29 13:54:44 +0300

    Finish

--- contacts.js
+++ contacts.js
@@ -1,27 +1,57 @@
 const fs = require("fs/promises");
-fs.readFile("db/contacts.json", "utf-8").then(data => console.log(data))
+fs.readFile("db/contacts.json", "utf8").then(data => console.log(data))
   .catch(error => console.log(error.mesage))
-
+const { nanoid } = require("nanoid");
 const path = require("path");
-const contactsPath = path.join(__dirname, "contacts.json");
-  /*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+const contactsPath = path.join(__dirname, "/db/contacts.json");
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  // ...твой код
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const allContacts = JSON.parse(data);
+  return allContacts;
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  const allContacts = await listContacts();
+  const contactToFind = allContacts.find((el) => el.id === contactId);
+  if (!contactToFind) {
+    console.log(`contact with id ${contactId} haven't been found`);
+    return;
+  }
+  console.log(`contact`, contactToFind);
+  return contactToFind;
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  const allContacts = await listContacts();
+  const contactToDelete = allContacts.find((el) => {
+    return el.id === contactId;
+  });
+  const actualizedContacts = allContacts.filter((el) => {
+    return el.id !== contactId;
+  });
+  await fs.writeFile(contactsPath, JSON.stringify(actualizedContacts));
+  console.log(contactToDelete);
+  return contactToDelete;
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  const allContacts = await listContacts();
+  const actualizedContacts = [
+    ...allContacts,
+    {
+      id: nanoid(),
+      name,
+      email,
+      phone
+    },
+  ];
+  await fs.writeFile(contactsPath, JSON.stringify(actualizedContacts));
+
+  console.table(actualizedContacts);
+  return actualizedContacts;
 }
+
+const operations = { listContacts, getContactById, removeContact, addContact };
+
+module.exports = operations;
